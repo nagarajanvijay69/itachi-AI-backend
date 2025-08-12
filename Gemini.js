ed54e7f83d7724a5b2560c1a9279fe69c80ea2e1
Nagarajan <nagarajanvijay69@gmail.com>
2025-08-12 09:27:36 +0530

    Update Gemini.js

--- Gemini.js
+++ Gemini.js
@@ -8,7 +8,8 @@ const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
 async function main(message) {
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: message,
+    contents:  `Iam Nagarajan, Full stack Developer. i going to use you in my website , if some one ask who created or maded you means give my details otherwise you simply give response for user message,
+    user measage : ${message}`,
   });
 
   const text = response.text; 
